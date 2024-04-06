64855a77165bf539d9407e0b3c8d45c482e1b803
Matheus Fernandes <matheus.mendescf@gmail.com>
2024-04-06 15:17:13 -0300

    Change copy

--- components/Utils/ContactForm/ContactDetails.jsx
+++ components/Utils/ContactForm/ContactDetails.jsx
@@ -5,7 +5,7 @@ export default function ContactDetails() {
     <div className="py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
       <div className="max-w-lg mx-auto">
         <p className="mt-3 text-lg leading-6 text-gray-500 dark:text-gray-200">
-          Se gostou do conteúdo e quer saber das proximas postagens, utilize uma das opções ao lado,
+          Se gostou do conteúdo e quer saber das proximas postagens, utilize uma das opções,
           ou volte daqui a tempo e descubra.
         </p>
         <dl className="mt-8 text-base text-gray-500 dark:text-gray-300">
