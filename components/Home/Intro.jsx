7495d52166070d56fad5227e20a417efd5f690a8
Matheus Fernandes <matheus.mendescf@gmail.com>
2024-04-08 08:57:45 -0300

    Add reflections to intro page

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -1,12 +1,19 @@
+import { REFLECTIONS } from '../../lib/constants'
+
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl lg:text-9xl font-bold tracking-tighter leading-tight md:pr-8">
         Surf.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Vida e reflexões de um Surfista.
-      </h4>
+      <div className="flex-col">
+        <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+          Vida e reflexões de um Surfista.
+        </h4>
+        <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+          Reflexão da vez: <strong><i>{REFLECTIONS.slice(-1)}</i></strong>
+        </h4>
+      </div>
     </section>
   )
 }
